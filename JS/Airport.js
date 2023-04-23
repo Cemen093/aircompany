3b916f921f697d23c1cfdd320bbedceac78f6faa
Pylin Semen <CemenP93@gmail.com>
2023-04-23 13:59:54 +0300

    test code refactoring and fix

--- JS/Airport.js
+++ JS/Airport.js
@@ -1,7 +1,9 @@
 const PassengerPlane = require('./Planes/PassengerPlane');
 const MilitaryPlane = require('./Planes/MilitaryPlane');
 const ExperimentalPlane = require('./Planes/ExperimentalPlane');
+const PlaneFactory = require('./Planes/PlaneFactory');
 const TypesMilitaryPlane = require('./models/TypesMilitaryPlane');
+const ModelsPlane = require('./models/ModelsPlane');
 
 class Airport {
 
@@ -46,8 +48,8 @@ class Airport {
         if (!passengerPlanes.length){
             throw new Error('No planes');
         }
-        return passengerPlanes.reduce((maxCapacityPlane, plane) => {
-                return plane.passengersCapacity > maxCapacityPlane.passengersCapacity ? plane : maxCapacityPlane;
+        return passengerPlanes.reduce((planeWithMaxCapacity, plane) => {
+                return plane.maxPassengersCapacity > planeWithMaxCapacity.maxPassengersCapacity ? plane : planeWithMaxCapacity;
             }, passengerPlanes[0]);
     }
     
@@ -62,12 +64,37 @@ class Airport {
     }
     
     sortByMaxLoadCapacity() {
-        this._planes.sort((a, b) => a.minLoadCapacity - b.minLoadCapacity);
+        this._planes.sort((a, b) => a.maxLoadCapacity - b.maxLoadCapacity);
         return this;
     }
 
     static toString(planes) {
         return JSON.stringify(planes);
+    }
+
+    static createAllPlanesAndReturn(){
+        const planeFactory = new PlaneFactory();
+
+        let planes = [
+            planeFactory.create(ModelsPlane.BOEING_737),
+            planeFactory.create(ModelsPlane.BOEING_737_800),
+            planeFactory.create(ModelsPlane.BOEING_747),
+            planeFactory.create(ModelsPlane.AIRBUS_A320),
+            planeFactory.create(ModelsPlane.AIRBUS_A330),
+            planeFactory.create(ModelsPlane.EMBRAER_190),
+            planeFactory.create(ModelsPlane.SUKHOI_SUPERJET_100),
+            planeFactory.create(ModelsPlane.BOMBARDIER_CS300),
+            planeFactory.create(ModelsPlane.B_1B_LANCER),
+            planeFactory.create(ModelsPlane.B_2_SPIRIT),
+            planeFactory.create(ModelsPlane.B_52_STRATOFORTRESS),
+            planeFactory.create(ModelsPlane.F_15),
+            planeFactory.create(ModelsPlane.F_22),
+            planeFactory.create(ModelsPlane.C_130_HERCULES),
+            planeFactory.create(ModelsPlane.BELL_X_14),
+            planeFactory.create(ModelsPlane.RYAN_X_13_VERTIJET)
+        ];
+
+        return planes;
     }
 }
 
--- JS/index.js
+++ JS/index.js
@@ -1,30 +1,8 @@
 const Airport = require('./Airport');
-const PlaneFactory = require('./Planes/PlaneFactory');
-const ModelsPlane = require('./models/ModelsPlane');
 
 (function run() {
-    const planeFactory = new PlaneFactory();
 
-    let planes = [
-        planeFactory.create(ModelsPlane.BOEING_737),
-        planeFactory.create(ModelsPlane.BOEING_737_800),
-        planeFactory.create(ModelsPlane.BOEING_747),
-        planeFactory.create(ModelsPlane.AIRBUS_A320),
-        planeFactory.create(ModelsPlane.AIRBUS_A330),
-        planeFactory.create(ModelsPlane.EMBRAER_190),
-        planeFactory.create(ModelsPlane.SUKHOI_SUPERJET_100),
-        planeFactory.create(ModelsPlane.BOMBARDIER_CS300),
-        planeFactory.create(ModelsPlane.B_1B_LANCER),
-        planeFactory.create(ModelsPlane.B_2_SPIRIT),
-        planeFactory.create(ModelsPlane.B_52_STRATOFORTRESS),
-        planeFactory.create(ModelsPlane.F_15),
-        planeFactory.create(ModelsPlane.F_22),
-        planeFactory.create(ModelsPlane.C_130_HERCULES),
-        planeFactory.create(ModelsPlane.BELL_X_14),
-        planeFactory.create(ModelsPlane.RYAN_X_13_VERTIJET)
-    ];
-
-    let airport = new Airport(planes);
+    let airport = new Airport(Airport.createAllPlanesAndReturn());
     let militaryAirport = new Airport(airport.militaryPlanes);
     let passengerAirport = new Airport(airport.passengerPlanes);
     console.log(`Military airport sorted by max distance: ${Airport.toString(militaryAirport.sortByMaxDistance())}`);
